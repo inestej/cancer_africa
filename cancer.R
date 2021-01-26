5e3c8ab786b458b9b21dfa73475b705bc66797b3
Ines Abdeljaoued Tej <32552388+inestej@users.noreply.github.com>
2021-01-26 22:15:05 +0100

    Update cancer.R

--- cancer.R
+++ cancer.R
@@ -1,4 +1,3 @@
-
 hdi <- c(0.682, 0.494, 0.543, 0.546, 0.641)
 names(hdi) <- c("Northern", "Western", "Central", "Eastern", "Southern")
 summary(hdi)
@@ -22,45 +21,60 @@ df
 colnames(df) <- c("Region", "HDI", "Cancer", "FR")
 df
 
-#method = "lm", color = "chocolate", span = 0.9, se=TRUE, fullrange=TRUE
-
-s <- ggplot(df, aes(x=HDI, y=FR, shape=Cancer, color=Region))
-s <- s + geom_point() #+ geom_label(colour="darkred", fontface = "bold", label=rownames(df))
-s <- s + stat_smooth(formula=y~x, method="lm", level = 0.25, 
-                     color="darkgreen", fill="darkgreen", span=0.9, se=TRUE, fullrange=TRUE)
-s <- s + annotate("text",colour='chocolate', x=0.62, y=75, label= paste("P-value=",pv4,"\n","rSquared=",rSquared4))+
-  labs(title = "HDI versus Cancer (Breast, Prostate\n and Cervical) Fatality Rate in Africa",
-       caption = "Data source: Globocan 2018 and UNESCO")+
-  xlab("Human Developpement Index 2018") + ylab("Cancer Fatality Rate per Africa region")
-s + geom_vline(xintercept = 0.56, linetype="dotted", 
-               color = "blue", size=0.5)+
-  annotate("text",colour='blue', x=0.52, y=24, label="Low HDI")+
-  annotate("text",colour='blue', x=0.61, y=24, label="Medium HDI")
-ggsave("hdi.png")
-
-s <- ggplot(df, aes(x=HDI, y=FR), shape=Cancer)
-s <- s + geom_point(aes(color=Cancer)) 
-s <- s + stat_smooth(method="lm", level = 0.25, color="darkred", fill="blue", se=TRUE, fullrange=TRUE) 
-s <- s + annotate("text",colour='chocolate', x=0.62, y=75, 
-                  label= paste("P-value=", pv4,"\n", "rSquared=", rSquared4)) +
-  labs(title="HDI versus Cancer (Breast, Prostate\n and Cervical) Fatality Rate in Africa",
-       caption="Data source: Globocan 2018 and UNESCO")+
-  xlab("Human Developpement Index 2018") + ylab("Cancer Fatality Rate per Africa region")
-s + geom_vline(xintercept = 0.56, linetype="dotted", 
-               color = "blue", size=0.5)+
-  annotate("text",colour='blue', x=0.52, y=24, label="Low HDI")+
-  annotate("text",colour='blue', x=0.61, y=24, label="Medium HDI")
-ggsave("hdi1.png")
 
 s <- ggplot(df, aes(x=HDI, y=FR, color=Region))
-s <- s + geom_point(aes(shape=Cancer)) 
+s <- s + geom_point(aes(shape=Cancer), size=2) 
 s <- s + stat_smooth(method = "lm", color = "chocolate", span = 0.9, se=TRUE, fullrange=TRUE) 
-s <- s + annotate("text",colour='chocolate', x=0.62, y=65, label= paste("P-value=",pv4,"\n","rSquared=",rSquared4))+
-  labs(title = "HDI versus Cancer Fatality Rate\n of Cervical and Prostate in Africa",
-       caption = "Data source: Globocan 2018 and UNESCO")+
-  xlab("Human Developpement Index 2018") + ylab("Cancer Fatality Rate per Africa region")
+s <- s + #annotate("text",colour='chocolate', x=0.62, y=65, label= paste("P-value=",pv4,"\n","rSquared=",rSquared4))+
+  labs(title = "HDI versus Cancer (Breast, Prostate\n and Cervical) Fatality Rate in Africa",
+       caption = "Data source: Globocan 2018 and UNESCO") +
+  xlab("Human Developpement Index (2018)") + ylab("FR (%)")
 s + geom_vline(xintercept = 0.56, linetype="dotted", color = "blue", size=0.5)+
-  annotate("text",colour='blue', x=0.52, y=24, label="Low HDI")+
-  annotate("text",colour='blue', x=0.61, y=24, label="Medium HDI")
-ggsave("hdi2.png")
+  annotate("text",colour='blue', x=0.52, y=20, label="Low HDI") +
+  annotate("text",colour='blue', x=0.61, y=20, label="Medium HDI") #+
+#scale_x_continuous("My x label") +
+#scale_y_continuous("My y label") 
+ggsave("hdi.png")
+
+
+hdi <- c(0.682, 0.494, 0.543, 0.546, 0.641)
+names(hdi) <- c("Northern", "West", "Central", "East", "Southern")
+summary(hdi)
+prostate <- c(13.2, 31.9, 35.9, 23.9, 64.1)
+breast <- c(48.9, 37.3, 27.9, 29.9, 46.2)
+cervical <- c(7.2, 29.6, 26.8, 40.1, 43.1)
+region <- c("Northern", "Western", "Central", "Eastern", "Southern")
+#df <- data.frame(hdi, prostate, breast, cervical, region)
+hdi <- rep(c(0.682, 0.494, 0.543, 0.546, 0.641), c(3,3,3,3,3))
+region <- rep(c("Northern", "Western", "Central", "Eastern", "Southern"), c(3,3,3,3,3))
+dp <- data.frame(region, hdi)
+cancer <- c("Breast", "Prostate", "Cervical", "Breast", "Prostate", "Cervical", "Breast", "Prostate", "Cervical", "Breast", "Prostate", "Cervical", "Breast", "Prostate", "Cervical")
+dp <- cbind(dp, cancer)
+str(dp)
+#attach(cancer)
+dp <- dp[order(cancer, decreasing = FALSE),]
+dp
+dp <- cbind(dp, c(breast, cervical, prostate))
+
+dp
+colnames(dp) <- c("Region", "HDI", "Cancer", "IR")
+dp
+
+model2 = lm(data=dp, formula=IR~HDI)
+res_model2 = anova(model2)
+pv3 = round(res_model2$`Pr(>F)`[1],5)
+rSquared3 <- round(summary(model2)$r.squared,2)
+
+s <- ggplot(dp, aes(x=HDI, y=IR, color=Region))
+s <- s + geom_point(aes(shape=Cancer), size=2) 
+s <- s + stat_smooth(method = "lm", color = "chocolate", span = 0.9, se=TRUE, fullrange=TRUE) 
+s <- s + #annotate("text",colour='chocolate', x=0.62, y=60, label= paste("P-value=", pv3,"\n","rSquared=", rSquared3)) +
+  labs(title = "HDI versus Cancer (Breast, Prostate\n and Cervical) Incidence Rate in Africa",
+       caption = "Data source: Globocan 2018 and UNESCO")+
+  xlab("Human Developpement Index (2018)") + ylab("IR (per 100 000 inhabitant)")
+s + geom_vline(xintercept = 0.56, linetype="dotted", color = "blue", size=0.5)+
+  annotate("text",colour='blue', x=0.52, y=17, label="Low HDI")+
+  annotate("text",colour='blue', x=0.61, y=17, label="Medium HDI")
+ggsave("hdi1.png")
+
 
